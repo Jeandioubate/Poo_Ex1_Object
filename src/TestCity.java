8f5282dcaeea06548766f18830b6c3ec0ea2a460
Jean Dioubaté <jean.dioubate@fms-ea.com>
2025-11-13 14:48:05 +0100

    ajout du code pour compter le nombre d'instances de la classe City, regroupement des questions 1.2 à 1.7 dans un seul <main>, changement de l'instance <toulouse> au niveau de l'exo 1.4 et 1.6 <tlse>.

--- src/TestCity.java
+++ src/TestCity.java
@@ -26,7 +26,7 @@ public class TestCity {
 
 	}
 */
-/*	//1.2
+	//1.2
 	// Nouvelles instances
 	public static void main(String[] args) {
 		City toulouse = new City("Toulouse", "France", 470000);
@@ -43,20 +43,17 @@ public class TestCity {
 		System.out.println("Nombre d'habitants actuel : " + toulouse.getNbInhabitants());
 		
 	// Je fais le test avec une valeur valide	
-		System.out.println("\n--- Test avec une valeur valide ---");
-		toulouse.setNbInhabitants(480000);
-		System.out.println("Nombre d'habitants actuel après changement : " + toulouse.getNbInhabitants());
+	//	System.out.println("\n--- Test avec une valeur valide ---");
+	//	toulouse.setNbInhabitants(480000);
+	//	System.out.println("Nombre d'habitants actuel après changement : " + toulouse.getNbInhabitants());
 		
 	// Je procède à la réduction de la population	
 		System.out.println("\n--- Réduire Toulouse de 470000 à 370000 habitants ---");
 		toulouse.reducePopulation(100000);
 		System.out.println("Population après réduction : " + toulouse.getNbInhabitants());		
-		
-	}
-*/
-/*	//1.3
-	// Test avec le nouveau constructeur prenant deux paramètres
-	public static void main(String[] args) {
+
+	//1.3
+	// Test avec le nouveau constructeur prenant deux paramètres	
 		City rabat = new City("Rabat", 577000);
 		System.out.println("--- Affichage après création avec 2 paramètres ---");
 		rabat.displayInfo();
@@ -67,24 +64,24 @@ public class TestCity {
 		System.out.println("Après modification : " + rabat.getCountry());
 		System.out.println("---- Affichage final ----");
 		rabat.displayInfo();
-	}
-*/
+
 	//1.4
-	//Création d'une nouvelle méthode
-	public static void main(String[] args) {
-		City toulouse = new City("Toulouse", "France", 450000);
+	//Création d'une nouvelle méthode	
+		City tlse = new City("Toulouse", "France", 450000);
 		System.out.println("--- Affichage des nouvels attributs de toulouse ---");
-		toulouse.display();	
+		tlse.display();	
 	//1.5
 	//	System.out.println(toulouse); // Le programme affiche un code. Parce qu'on a pas définie au préalable la méthode "toString()"
 		
 	//1.6	
 	// Affichage avec la méthode "toString()"
 		System.out.println("---- Affichage avec la méthode toString() ----");
-		System.out.println(toulouse);
-	}
+		System.out.println(tlse);
 	
-	
-		
+	//1.7 
+	//Compter le nombre d'instance de la classe "City"
+		System.out.println("--- Compteur du nombre d'instances de la classe City ---");
+		System.out.println("Le nombre d'instances de la classe City est : " + City.getcountInstance());
+	}		
 
 }
